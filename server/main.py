219d6e077c7f3af5bbb4ae7722a341b93b6fbf23
chenliang <chenliang@tencent.com>
2025-08-07 10:50:48 +0800

    [feat] add user manually

--- server/main.py
+++ server/main.py
@@ -1,1 +1,24 @@
 from app_factory import create_app
+
+if __name__ == '__main__':
+    async def add_user(args):
+        from core.account import CoreAccount
+        from util.database import create_db_engine
+
+        _, sessionmaker = create_db_engine(app)
+        db = sessionmaker()
+        account = await CoreAccount.create_account(db, email=args.u, password=args.p)
+        await db.close()
+
+    app = create_app()
+    import asyncio
+    import argparse
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--add-user', action='store_true')
+    parse.add_argument('-u', type=str)
+    parse.add_argument('-p', type=str)
+    args, _ = parse.parse_known_args()
+    print(args)
+    if args.add_user:
+        asyncio.run(add_user(args))
+        exit(0)
