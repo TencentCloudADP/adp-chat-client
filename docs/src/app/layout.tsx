c04e42314353459da05c0002deaac95f9b2a8f35
TencentCloudADP-DevRel <adp_devrel@tencent.com>
2025-09-30 12:06:38 +0800

    Revert "gh pages"

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
-import { GoogleAnalytics } from '@/components/analytics';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -10,9 +9,6 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <head>
-        <GoogleAnalytics />
-      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
