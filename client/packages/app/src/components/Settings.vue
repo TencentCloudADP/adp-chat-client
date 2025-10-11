41f0dde66ddcee56896cc39373ff3249dce819ed
denniskang <denniskang@tencent.com>
2025-10-11 15:06:22 +0800

    feat(client): Implement settings menu

--- client/packages/app/src/components/Settings.vue
+++ client/packages/app/src/components/Settings.vue
@@ -1,18 +1,75 @@
-<script setup lang="ts">
+<script setup lang="tsx">
+import { languageMap } from '@/i18n';
+import { useUiStore } from '@/stores/ui';
+import { useRouter } from 'vue-router';
+import { logout } from '@/service/login';
 
-const toggleSettingPanel = () => {
-    // Implement logic to toggle the settings panel
-    console.log('Toggling settings panel...');
+const router = useRouter();
+const uiStore = useUiStore();
+
+/**
+ * 切换主题模式（light <-> dark）。
+ */
+const toggleTheme = () => {
+    const previousTheme = uiStore.theme;
+    const newTheme = previousTheme === 'light' ? 'dark' : 'light';
+    uiStore.setTheme(newTheme);
 };
 
+const handleLogout = () => {
+    logout(() => router.replace('/login'));
+};
 </script>
 
 <template>
-    <t-tooltip placement="bottom" :content="$t('header.systemSettings')">
-        <t-button theme="default" shape="square" variant="text" @click="toggleSettingPanel">
-            <t-icon name="setting-1" />
-        </t-button>
-    </t-tooltip>
+    <t-space>
+        <t-dropdown maxColumnWidth="300px">
+            <t-button theme="default" shape="square" variant="text">
+                <t-icon name="setting-1" />
+            </t-button>
+            <t-dropdown-menu>
+                <t-dropdown-item>
+                    <div @click="toggleTheme" class="dropdown-item">
+                        <t-icon :name="uiStore.theme === 'light' ? 'mode-light' : 'moon'" />
+                        {{ $t('sider.switchTheme') }}
+                    </div>
+                </t-dropdown-item>
+
+                <t-dropdown-item>
+                    <div class="dropdown-item">
+                        <t-icon name="translate" />
+                        {{ $t('sider.selectLanguage') }}
+                    </div>
+                    <t-dropdown-menu>
+                        <t-dropdown-item v-for="(value, key) in languageMap" :key="key"
+                            @click="uiStore.setLanguage(key)">
+                            <div class="operations-dropdown-container-item">
+                                {{ value }}
+                            </div>
+                        </t-dropdown-item>
+                    </t-dropdown-menu>
+                </t-dropdown-item>
+
+                <t-dropdown-item>
+                    <div @click="handleLogout" class="dropdown-item">
+                        <t-icon name="poweroff"></t-icon>
+                        {{ $t('account.logout') }}
+                    </div>
+                </t-dropdown-item>
+            </t-dropdown-menu>
+        </t-dropdown>
+    </t-space>
 </template>
 
-<style scoped></style>
+<style scoped>
+.dropdown-item {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    gap: var(--td-comp-paddingLR-m);
+}
+
+.t-icon {
+    font-size: 20px;
+}
+</style>
