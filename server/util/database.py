c586ae5e3b420134e282a5590a6c6f204a084a5c
chenliang <chenliang@tencent.com>
2025-11-10 10:39:47 +0800

    fix(server): log an error message if the 'Vender' config was not found

--- server/util/database.py
+++ server/util/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app_factory import TAgenticApp
+from config import tagentic_config
 
 
 def create_db_engine(app: TAgenticApp, override_db: str = None):
@@ -11,6 +12,6 @@ def create_db_engine(app: TAgenticApp, override_db: str = None):
         f'{app.config.PGSQL_USER}:{app.config.PGSQL_PASSWORD}@'
         f'{app.config.PGSQL_HOST}:{app.config.PGSQL_PORT}/{db}'
     )
-    db_engine = create_async_engine(f"postgresql+asyncpg://{db_config}", echo=True)
+    db_engine = create_async_engine(f"postgresql+asyncpg://{db_config}", echo=(tagentic_config.LOG_LEVEL=='DEBUG'))
     _sessionmaker = sessionmaker(db_engine, class_=AsyncSession, expire_on_commit=False)
     return db_engine, _sessionmaker
