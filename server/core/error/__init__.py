566625c3957664d27f17c5dffbf5ef65ff8ec4f3
chenliang <chenliang@tencent.com>
2025-12-17 11:37:54 +0800

    fix(server): call super init in BaseError

--- server/core/error/__init__.py
+++ server/core/error/__init__.py
@@ -4,4 +4,4 @@ from sanic.exceptions import SanicException
 
 class BaseError(SanicException):
     def __init__(self, description: Optional[str] = None):
-        self.description = description
+        super().__init__(description)
