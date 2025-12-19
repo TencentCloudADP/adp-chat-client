110d76f3a910af4b771f185d20bf1626a3d7ee75
chenliang <chenliang@tencent.com>
2025-12-19 19:38:23 +0800

    bump: version 0.2.0 → 0.2.1

--- client/packages/app0/vite.config.ts
+++ client/packages/app0/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
@@ -6,7 +6,9 @@ import path from 'path'; //导入已经安装的@type/node
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
   define: {
     __VUE_PROD_DEVTOOLS__: true,
   },
@@ -30,4 +32,19 @@ export default defineConfig({
   build: {
     commonjsOptions: { include: [/markdown-it-texmath/, /node_modules/] },
   },
-})
+  optimizeDeps: {
+    include: ['markdown-it-texmath'],
+  },
+  server: {
+    proxy: {
+      '/application': {
+        target: env.SERVICE_API_URL,
+        changeOrigin: true,
+      },
+      '/chat': {
+        target: env.SERVICE_API_URL,
+        changeOrigin: true,
+      },
+    },
+  },
+}})
