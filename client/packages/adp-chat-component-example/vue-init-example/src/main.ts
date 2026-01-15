33a9c1d7a17120fd3a8990119613a1d7b32e12e1
suyaliang <suyaliang@tencent.com>
2026-01-16 02:10:52 +0800

    fix(client): resolve compilation errors in production build​

--- client/packages/adp-chat-component-example/vue-init-example/src/main.ts
+++ client/packages/adp-chat-component-example/vue-init-example/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import 'tdesign-vue-next/es/style/index.css'
-import 'virtual:svg-icons-register'
 import './assets/main.css'
 import App from './App.vue'
 import { configureAxios } from 'adp-chat-component'
