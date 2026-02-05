74f6b7b3cc5b57d1b12aef4bad7d2aea581d344b
chenliang <chenliang@tencent.com>
2026-02-05 17:24:45 +0800

    fix(server): fix get_remote_ip, the limiter works as expected

--- server/util/helper.py
+++ server/util/helper.py
@@ -14,7 +14,7 @@ def get_remote_ip(request: Request) -> str:
     if request.headers.get("X-Forwarded-For"):
         return cast(str, request.headers.get("X-Forwarded-For"))
     else:
-        return cast(str, request.remote_addr)
+        return cast(str, request.client_ip)
 
 
 def get_path_base() -> str:
